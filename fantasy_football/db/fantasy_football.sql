f159ecc89de450c52da43cdb5abd0bbb441887f3
Jackie Farr <jacquelynfarr@hotmail.com>
2018-10-05 14:52:54 +0100

    first commit

---
+++ fantasy_football/db/fantasy_football.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS players;
+DROP TABLE IF EXISTS teams;
+
+CREATE TABLE teams
+(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE players
+(
+  id SERIAL4 PRIMARY KEY,
+  first_name VARCHAR(255) not null,
+  last_name VARCHAR(255) not null,
+  team_id INT4 references teams(id),
+  position VARCHAR(255) not null,
+  salary VARCHAR(255) not null,
+  injury VARCHAR(255) not null,
+  goals INT4
+)
